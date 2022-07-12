f96f2643940163ed81507b278a80728c145aaa22
1N0Face <maxim.black2004@gmail.com>
2022-07-12 09:40:22 +0300

    added cameras bash script

---
+++ cameras.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gnome-terminal --tab -- bash -c "vlc rtsp://admin:UOXBGL@192.168.0.233:554/H.264"
+gnome-terminal --tab -- bash -c "vlc rtsp://admin:TGASLM@192.168.0.76:554/H.264"
+gnome-terminal --tab -- bash -c "vlc rtsp://admin:VSYAJL@192.168.0.79:554/H.264"
+gnome-terminal --tab -- bash -c "vlc rtsp://admin:IGXJVS@192.168.0.55:554/H.264"
